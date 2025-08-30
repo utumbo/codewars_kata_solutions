361252c70734d94027d9e116e289bd96df869e63
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-30 14:32:26 +0700

    create are you playing banjo
    create are you playing banjo

---
+++ 8kyu/Are_You_Playing_Banjo.py
@@ -0,0 +1,7 @@
+def are_you_playing_banjo(name):
+    if name[0].lower() == 'r':
+        return f'{name} plays banjo'
+    else:
+        return f'{name} does not play banjo'
+    
+# https://www.codewars.com/kata/53af2b8861023f1d88000832/train/python
